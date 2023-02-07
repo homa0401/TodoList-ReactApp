a18b23af81d75d5cc78c383bc1044b2cf75e94c5
homa0401 <humoyun0401@gmail.com>
2023-02-07 16:12:57 +0900

    new feature

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,7 +1,35 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "rgb(243, 157, 157)" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+  }
+`;
 
 const TaskInput = (props) => {
   const [inputText, setInputText] = useState("");
@@ -25,10 +53,10 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <FormControl invalid={!isInputValid}>
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,32 @@
-import "./Button.css";
+import styled from "styled-components";
 
-const Button = (props) => {
-  return (
-    <button type={props.type} className="button">
-      {props.children}
-    </button>
-  );
-};
+// tagged template literals
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  color: white;
+  background: #00358b;
+  border-radius: 4px;
+  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #245fbd;
+    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+  }
+`;
+
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button">
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
